f2eecdb1434d8163f84cd28907b0b2772d4d524e
Nick Freear <nfreear@yahoo.co.uk>
2026-04-13 18:12:50 +0100

    feat: Add extended AD for UN-CRPD disability rights video (YouTube)

--- src/util/videoData.js
+++ src/util/videoData.js
@@ -2,13 +2,23 @@ const DATA = [
   {
     id: 'vbde1',
     title: 'Christmas at …',
+    rights: '© All rights reserved.',
     mediaUrl: 'https://youtu.be/HgHj3g68Tr4',
     trackUrl: '../tracks/visit-britain-downton.ext-ad.en.vtt',
+    provider: 'youtube'
+  }, {
+    id: 'uncrpd1',
+    title: 'Committee on the Rights of Persons with Disabilities (CRPD), by UN Human Rights',
+    rights: 'License: CC:BY',
+    mediaUrl: 'https://youtu.be/dX2zmMSLkj4',
+    trackUrl: '../tracks/un-crpd.ext-ad.en.vtt',
+    moreUrl: 'https://www.ohchr.org/TreatyBodies',
     provider: 'youtube'
   }, {
     // Important: put 'default' last!
     id: 'default',
     title: 'Sailing Bib Bear Lake from the sky, by Tim Hanley',
+    rights: 'License: Public Domain (CC0)',
     mediaUrl: 'https://vimeo.com/1006361470',
     trackUrl: '../tracks/sailing-bib-bear.ext-ad.en.vtt',
     provider: 'vimeo'
