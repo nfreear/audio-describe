43390f9964adb23402762aff5f183becad8f603e
Nick Freear <nfreear@yahoo.co.uk>
2026-05-25 14:57:27 +0100

    feat: Integrated `<my-include>` custom element for footer
    * https://github.com/nfreear/elements.git

--- demo/lib/app.js
+++ demo/lib/app.js
@@ -1,3 +1,10 @@
 import DemoAppElement from '../../src/util/DemoAppElement.js';
+import MyIncludeElement from 'ndf-elements/my-include';
+
+const myIncludeEl = document.querySelector('my-include');
+myIncludeEl && myIncludeEl.addEventListener('ready', (ev) => {
+  console.debug('my-include ready:', [ev.target], ev);
+});
 
 window.customElements.define('demo-app', DemoAppElement);
+window.customElements.define('my-include', MyIncludeElement);
